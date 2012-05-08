b566747e19a029407656f344fc7f6ae21d214f01
ios2686 <ios2686@caelum-macmini03.(none)>
2012-05-08 18:51:22 -0300

    Initial Commit

---
+++ FirstContacts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstContacts
+//
+//  Created by ios2686 on 08/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
